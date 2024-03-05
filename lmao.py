14bc74d7b45ece24cf0616b1310e79d4c97114eb
hung2002741 <mathewandharrrypotter@gmail.com>
2024-03-05 10:35:30 +0700

    IT's all my money

--- lmao.py
+++ lmao.py
@@ -7,13 +7,13 @@ def soluong(a, b):
 
 
 def luonglamloilon():
-    tien_1 = input("tien luong 1: ")
-    tien_2 = input("tien luong 2: ")
+    tien_1 = input("tien luong 1 cua Hung: ")
+    tien_2 = input("tien luong 2 cua Hung: ")
     return tien_1, tien_2
 
 
 tien_1, tien_2 = luonglamloilon()
 
-soluong = soluong(tien_1, tien_2)
+soluong = soluong(tien_1*100, tien_2*100)
 
 print(soluong)
